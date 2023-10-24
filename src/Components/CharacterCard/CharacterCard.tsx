e9df3f36710446fc09b2de00fa9623f3d88f1757
Patricia <patricia.femenias@gmail.com>
2023-10-24 23:23:14 +0200

    Modify CharacterCard structure

--- src/Components/CharacterCard/CharacterCard.tsx
+++ src/Components/CharacterCard/CharacterCard.tsx
@@ -1,4 +1,5 @@
 import { Character } from "../../types";
+import "./CharacterCard.css";
 
 interface CharacterCardProps {
   character: Character;
@@ -9,16 +10,25 @@ const CharacterCard = ({
 }: CharacterCardProps): React.ReactElement => {
   return (
     <div className="character-card">
-      <h2 className="character-card__name">{character.name}</h2>
       <img
         className="character-card__image"
         src={character.picture}
         alt={`${character.name} Star Wars Character`}
       />
-      <ul className="caracter-card__data">
-        <li>Height:{character.name}</li>
-        <li>Mass:{character.mass}</li>
-        <li>BirthYear:{character.birthYear}</li>
+      <h2 className="character-card__name">{character.name}</h2>
+      <ul className="character-card__data-container">
+        <li className="character-card__title-data">
+          Height:
+          <span className="character-card__data">{character.height}</span>
+        </li>
+        <li className="character-card__title-data">
+          Mass:
+          <span className="character-card__data">{character.mass}</span>
+        </li>
+        <li className="character-card__title-data">
+          BirthYear:
+          <span className="character-card__data">{character.birthYear}</span>
+        </li>
       </ul>
     </div>
   );
