b06e9d3549e620ca5d8a6b967b49ab9fe8c92d43
Matias Risco <matiasriscolagos@gmail.com>
2023-10-24 19:49:53 +0200

    Add funtion getUrlImage in folder data

--- src/data/functions.tsx
+++ src/data/functions.tsx
@@ -4,3 +4,8 @@ export const fetchData = async (url: string): Promise<StarWarsApi> => {
   const response = await fetch(url);
   return response.json();
 };
+
+export const getUrlImage = (url: string): string => {
+  const imageId = Number(url.split("/").at(-2)!);
+  return `https://starwars-visualguide.com/assets/img/characters/${imageId}.jpg`;
+};
