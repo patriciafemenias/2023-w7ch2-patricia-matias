a9ba59e7ff07146307dc077095e978ad9ab19f42
Matias Risco <matiasriscolagos@gmail.com>
2023-10-24 21:00:38 +0200

    Add test of function getIdAndUrlImage

---
+++ src/data/getIdAndUrlImage/getIdAndUrlImage.test.ts
@@ -0,0 +1,17 @@
+import { getIdAndUrlImage } from "../functions";
+
+describe("Given a getIdAndUrlImage function", () => {
+  const url = "https://swapi.dev/api/people/2/";
+  describe(`When given url ${url}`, () => {
+    test("It return [2,'https://starwars-visualguide.com/assets/img/characters/2.jpg']", () => {
+      const expectedValue = [
+        2,
+        "https://starwars-visualguide.com/assets/img/characters/2.jpg",
+      ];
+
+      const actualValue = getIdAndUrlImage(url);
+
+      expect(actualValue).toStrictEqual(expectedValue);
+    });
+  });
+});
