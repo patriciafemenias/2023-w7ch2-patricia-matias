d8bf705713537e3fb2a1a0bde6df80861660568a
Patricia <patricia.femenias@gmail.com>
2023-10-24 23:51:47 +0200

    Fix CharactersList naming

--- src/Components/CharactersList/CharactersList.tsx
+++ src/Components/CharactersList/CharactersList.tsx
@@ -1,15 +1,16 @@
 import { Character } from "../../types";
 import CharacterCard from "../CharacterCard/CharacterCard";
+import "./CharactersList.css";
 
 interface ListCharacterprops {
   characters: Character[];
 }
 
-const ListCharacter = ({
+const CharactersList = ({
   characters,
 }: ListCharacterprops): React.ReactElement => {
   return (
-    <ul className="character-list">
+    <ul className="characters-list">
       {characters.map(
         (character): React.ReactElement => (
           <li key={character.id}>{<CharacterCard character={character} />}</li>
@@ -19,4 +20,4 @@ const ListCharacter = ({
   );
 };
 
-export default ListCharacter;
+export default CharactersList;
